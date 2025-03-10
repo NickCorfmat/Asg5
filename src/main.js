8773342709ac5d7c0852a82c4d2bc43d109feb7a
Nick Corfmat <ncorfmat@ucsc.edu>
2025-03-09 23:04:51 -0700

    more world building

--- src/main.js
+++ src/main.js
@@ -50,7 +50,7 @@ export function main() {
       0xffffff,
       10,
       100,
-      Math.PI / 4,
+      degToRad(45),
       0.2,
       0.5
     );
@@ -68,12 +68,22 @@ export function main() {
   // Ground Plane
   {
     const size = 500;
+
     const groundGeometry = new THREE.PlaneGeometry(size, size, 32, 32);
-    groundGeometry.rotateX(-Math.PI / 2);
+    groundGeometry.rotateX(degToRad(-90));
+
+    const texture = textureLoader.load("../assets/images/sidewalk.png");
+
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(150, 150);
+
     const groundMaterial = new THREE.MeshStandardMaterial({
-      color: 0x808080,
+      map: texture,
+      color: 0xb8b8b8,
       side: THREE.DoubleSide,
     });
+
     const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
     groundMesh.castShadow = false;
     groundMesh.receiveShadow = true;
@@ -139,19 +149,19 @@ export function main() {
       mesh.scale.set(scale, scale, scale);
 
       const instances = [
-        { x: 0.7, z: 63, rotation: 0 }, // Front
-        { x: 0.7, z: 80, rotation: 0 },
-        { x: 0.7, z: -63, rotation: 0 }, // Back
-        { x: 0.7, z: -80, rotation: 0 },
-        { x: 0.7, z: -100, rotation: 0 },
-        { x: 0.7, z: -120, rotation: 0 },
-        { x: 63, z: 0.7, rotation: (3 * Math.PI) / 2 }, // Left
-        { x: 80, z: 0.7, rotation: (3 * Math.PI) / 2 },
-        { x: -63, z: 0.7, rotation: (3 * Math.PI) / 2 }, // Right
-        { x: -80, z: 0.7, rotation: (3 * Math.PI) / 2 },
-        { x: -100, z: 0.7, rotation: (3 * Math.PI) / 2 },
-        { x: -120, z: 0.7, rotation: (3 * Math.PI) / 2 },
-        { x: -140, z: 0.7, rotation: (3 * Math.PI) / 2 },
+        { x: 0.7, z: 63, rotation: degToRad(0) }, // Front
+        { x: 0.7, z: 80, rotation: degToRad(0) },
+        { x: 0.7, z: -63, rotation: degToRad(0) }, // Back
+        { x: 0.7, z: -80, rotation: degToRad(0) },
+        { x: 0.7, z: -100, rotation: degToRad(0) },
+        { x: 0.7, z: -120, rotation: degToRad(0) },
+        { x: 63, z: 0.7, rotation: degToRad(270) }, // Left
+        { x: 80, z: 0.7, rotation: degToRad(270) },
+        { x: -63, z: 0.7, rotation: degToRad(270) }, // Right
+        { x: -80, z: 0.7, rotation: degToRad(270) },
+        { x: -100, z: 0.7, rotation: degToRad(270) },
+        { x: -120, z: 0.7, rotation: degToRad(270) },
+        { x: -140, z: 0.7, rotation: degToRad(270) },
       ];
 
       for (let i = 0; i < instances.length; i++) {
@@ -182,10 +192,10 @@ export function main() {
 
       objLoader.load("../assets/models/city/city.obj", (mesh) => {
         const instances = [
-          { x: 105, z: 0, rotation: (3 * Math.PI) / 2 },
-          { x: -115, z: 100, rotation: 2 * Math.PI },
-          { x: 0, z: 150, rotation: (2 * Math.PI) / 5 },
-          { x: -50, z: -100, rotation: 3 * Math.PI },
+          { x: 105, z: 0, rotation: degToRad(270) },
+          { x: -115, z: 100, rotation: degToRad(360) },
+          { x: 0, z: 150, rotation: degToRad(72) },
+          { x: -50, z: -100, rotation: degToRad(540) },
         ];
 
         for (let i = 0; i < instances.length; i++) {
@@ -202,6 +212,36 @@ export function main() {
     });
   }
 
+  // London Building
+  // Source: https://sketchfab.com/3d-models/free-london-kinnaird-house-ff13d8c9407f4796886a689a758dc862
+  {
+    const scale = 1.8;
+
+    gltfLoader.load(
+      "../assets/models/londonbuilding/londonbuilding.gltf",
+      (gltf) => {
+        const mesh = gltf.scene;
+        mesh.scale.set(scale, scale, scale);
+
+        const instances = [
+          { x: -25, z: -32, rotation: degToRad(90) },
+          { x: 28, z: 35, rotation: degToRad(270) },
+        ];
+
+        for (let i = 0; i < instances.length; i++) {
+          const { x, z, rotation } = instances[i];
+          const instance = mesh.clone();
+
+          instance.scale.set(scale, scale, scale);
+          instance.position.set(x, -0.5, z);
+          instance.rotation.y = rotation;
+
+          scene.add(instance);
+        }
+      }
+    );
+  }
+
   // Traffic Cones
   {
     const scale = 0.04;
@@ -214,7 +254,7 @@ export function main() {
     const startZ = 12;
     const gap = 5;
 
-    const numCones = 10
+    const numCones = 10;
 
     for (let i = 0; i < numCones; i++) {
       const cone1 = new THREE.Mesh(geometry, material);
@@ -226,7 +266,40 @@ export function main() {
       cone2.position.set(startX2, 0.3, startZ + i * gap);
       cone2.scale.set(scale, scale, scale);
       scene.add(cone2);
+    }
   }
+
+  // Crates
+  {
+    const size = 0.8;
+    const geometry = new THREE.BoxGeometry(size, size, size);
+
+    const texture = textureLoader.load("../assets/images/crate.png");
+    texture.colorSpace = THREE.SRGBColorSpace;
+
+    const material = new THREE.MeshStandardMaterial({
+      map: texture,
+    });
+
+    const instances = [
+      { x: 15, y: 0, z: -11, rotation: 0 },
+      { x: 13, y: 0, z: -10, rotation: degToRad(15) },
+      { x: 15, y: 0, z: -12.5, rotation: degToRad(50) },
+    ];
+
+    for (let i = 0; i < instances.length; i++) {
+      const { x, y, z, rotation } = instances[i];
+      const crate = new THREE.Mesh(geometry, material);
+
+      crate.position.set(x, y + 0.5, z);
+      crate.rotation.y = rotation;
+
+      scene.add(crate);
+    }
+  }
+
+  function degToRad(degrees) {
+    return (degrees * Math.PI) / 180;
   }
 
   // Resize Renderer to Fit Display
